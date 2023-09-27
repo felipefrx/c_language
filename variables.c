a1fdb4e1cf8b7a7dcc801f6d729261c5706d4599
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2023-09-27 16:30:53 -0300

    Add files via upload
    Adiciona arquivo da aula de variáveis.

---
+++ variables.c
@@ -0,0 +1,17 @@
+
+#define PI 3.1416
+
+int main(){
+
+    short a = 10;
+    int b = 1000;
+    long c = 100000000;
+    float d = 1.11111;
+    double e = 1.11111111111111;
+    char f = 'Text'; 
+    char x = PI; //Constante
+    double = (int)6.0; //Casting
+    unsigned int n; //Somente valores positivos 
+    n = 10;
+
+}
