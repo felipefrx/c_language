07d7a0bd11eb47514d4c5883251af4ed34f2b319
Felipe Ferreira <78969308+felipefrx@users.noreply.github.com>
2023-10-05 21:46:47 -0300

    Add files via upload

---
+++ scanfFunction.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int x;
+    printf("Type a number: ");
+    scanf("%d", &x);
+    printf("The number entered was: %d\n", x);
+
+    return 0;
+}
+
+    /*
+    "%d" - Decimal
+    "%ld" - Long Int
+    "%f" - Floating Point
+    "%lf" - Double
+    "%c" - Character
+    "%s" - Character Sequence
+    
+    */
