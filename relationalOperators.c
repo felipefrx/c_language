797f2eb759a8709e06c84d98fb3649bc2eb198d4
Felipe Ferreira <silvaferreirafelipe@gmail.com>
2023-10-09 10:54:36 -0300

    Adiciona aula de operadores relacionais

---
+++ relationalOperators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+main (){
+
+    int x = 10;
+    int y = 2;
+
+    printf("%d\n", x==y);
+
+}
+
+
+
+
+    /*
+        == : Equal
+        != : Different
+        >  : Bigger
+        <  : Smaller
+        >= : Bigger or equal
+        <= : Smaller or equal
+
+        && : AND
+        || : OR
+        !  : NOT 
+    */
